5536d813ab1f5ab9688fb9e7d679fd7e6f7cdf72
Adam Hooper <adam@adamhooper.com>
2020-03-10 08:18:37 -0700

    Initial commit: just files from cjworkbench/staticmodules/

---
+++ pastecsv.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from cjwparse.api import parse_csv
+import tempfile
+
+
+def render(arrow_table, params, output_path, **kwargs):
+    with tempfile.NamedTemporaryFile(suffix=".txt") as tf:
+        utf8_path = Path(tf.name)
+        utf8_path.write_text(params["csv"], encoding="utf-8")
+
+        return parse_csv(
+            utf8_path,
+            output_path=output_path,
+            encoding="utf-8",
+            delimiter=None,
+            has_header=params["has_header_row"],
+            autoconvert_text_to_numbers=True,
+        )
